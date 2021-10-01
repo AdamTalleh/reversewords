cd0e31786830ba189cea4e5f99dc11a48e9e8250
AdamTalleh <46764037+AdamTalleh@users.noreply.github.com>
2021-10-01 11:02:40 +0200

    ReverseWords
    reversewords v0.1

--- js/site.js
+++ js/site.js
@@ -1,5 +1,34 @@
-//call helloworld 
-function HelloWorld(){
-    alert("hello world!");
-     
+// get the string from the page
+//controller function
+function getValue(){
+    document.getElementById("alert").classList.add("invisible");
+    let userString = document.getElementById("userString").value;
+    let revString = reverseString(userString);
+    displayString(revString);
+}
+
+
+//reverse the string
+//logic function
+function reverseString(userString){
+
+let revString =[];
+
+//reverse a string using a for loop
+
+for (let index = userString.length -1; index >= 0; index--) {
+    revString += userString[index];
+    
+}
+return revString;
+}
+
+//display the reversed string to the user 
+//view function
+function displayString(revString){
+ // write to the page
+ document.getElementById("msg").innerHTML=`Your Reversed String is : ${revString}`;
+
+ //show the alert box
+ document.getElementById("alert").classList.remove("invisible");
 }
